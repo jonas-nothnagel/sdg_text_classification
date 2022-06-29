4f8ba0e5c182aae3b8a5e45e990aa8f0f245b683
jonas.nothnagel@gmail.com <jonas.nothnagel@gmail.com>
2022-06-29 16:04:37 +0200

    new bash script

--- run_example.sh
+++ run_example.sh
@@ -1,1 +1,10 @@
-python ./src/test.py
+#!/bin/bash
+
+#check environement
+#creat in doubt
+#pip install -r requirements.txt
+srun \
+  --container-image=/data/enroot/nvcr.io_nvidia_pytorch_22.05-py3.sqsh \
+  --container-workdir="`pwd`" \
+  --container-mounts=/data/nothnagel:/data/nothnagel/test_folder \
+  python ./src/test.py
