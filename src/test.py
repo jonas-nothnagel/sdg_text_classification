95a740c39d7e725de4cea3a9e19ed4edf7a90e96
jonas.nothnagel@gmail.com <jonas.nothnagel@gmail.com>
2022-06-29 14:48:17 +0200

    added bash test script

---
+++ src/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Test script for bashing on server
+"""
+
+import pandas as pd
+import joblib
+
+if __name__ == '__main__':
+    
+    print("test this script.....")
+
+    print("")
+
+    print("it works!!")
+
+    print("")
