60b6aaf3b3013112617f3ed6013a9e6cf6cf7495
jonas.nothnagel@gmail.com <jonas.nothnagel@gmail.com>
2022-06-29 17:07:07 +0200

    added env bash script

--- run_example.sh
+++ run_example.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# install requirements 
+pip install -r requirements.txt
+
 srun \
   --container-image=/data/enroot/nvcr.io_nvidia_pytorch_22.05-py3.sqsh \
   --container-workdir="`pwd`" \
