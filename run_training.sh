4bfc27f2165575e655d49b72fa218c6ed1f3d742
jonas.nothnagel@gmail.com <jonas.nothnagel@gmail.com>
2022-07-01 10:22:28 +0200

    new training script

--- run_training.sh
+++ run_training.sh
@@ -7,5 +7,5 @@ srun \
   --container-image=/data/enroot/nvcr.io_nvidia_pytorch_22.05-py3.sqsh \
   --container-workdir="`pwd`" \
   --container-mounts=/data/nothnagel/sdg_text_classification:/data/nothnagel/sdg_text_classification \
-python ./src/test.py
+python ./src/train.py
 
