daf4315a93a71cd9f480e762fac36b6c5c55e897
jonas.nothnagel@gmail.com <jonas.nothnagel@gmail.com>
2022-07-22 15:46:10 +0200

    modified training script

--- run_training.sh
+++ run_training.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-
-srun -K -N1 --gpus=8 --cpus-per-task=4 -p batch  \
---container-workdir=`pwd`  \
---container-mounts=/data/nothnagel/sdg_text_classification:/data/nothnagel/sdg_text_classification  \
---container-image=/data/enroot/nvcr.io_nvidia_pytorch_22.05-py3.sqsh  \
+pip install -r requirements.txt 
 python ./src/train.py 
