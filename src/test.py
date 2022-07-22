40ccae826bb2ded36445cf625a25067a10f0a108
jonas.nothnagel@gmail.com <jonas.nothnagel@gmail.com>
2022-07-22 12:37:22 +0200

    add torch test

--- src/test.py
+++ src/test.py
@@ -7,8 +7,12 @@ Test script for bashing on server
 
 import pandas as pd
 import joblib
+from torch import cuda
 
 if __name__ == '__main__':
+
+    device = 'cuda' if cuda.is_available() else 'cpu'
+    print(device)
     
     print("test this script.....")
 
