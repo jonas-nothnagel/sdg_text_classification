47b8fba43663530369048604dfacbf3b5f6e7c68
jonas.nothnagel@gmail.com <jonas.nothnagel@gmail.com>
2022-07-22 10:33:09 +0200

    changed scripts

--- run_example.sh
+++ run_example.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-srun \
-  --container-image=/data/enroot/nvcr.io_nvidia_pytorch_22.05-py3.sqsh \
-  --container-workdir="`pwd`" \
-  --container-mounts=/data/nothnagel/sdg_text_classification:/data/nothnagel/sdg_text_classification \
+pip install requirements.txt 
 python ./src/test.py
 
--- run_training.sh
+++ run_training.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 srun -K -N1 --gpus=8 --cpus-per-task=4 -p batch  \
 --container-workdir=`pwd`  \
 --container-mounts=/data/nothnagel/sdg_text_classification:/data/nothnagel/sdg_text_classification  \
